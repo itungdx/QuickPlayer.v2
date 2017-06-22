17c538fe1dccd2e6f18b55e095c754acaeb85112
Tung <tung@techmasters-iMac-2.local>
2017-06-22 10:26:53 +0700

    Initial Commit

---
+++ QuickPlayer.v2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuickPlayer.v2
+//
+//  Created by Tung on 6/22/17.
+//  Copyright © 2017 Tung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
